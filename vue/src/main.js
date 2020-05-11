1c3ce62d322ff19495df61331dbd3a42226edb2d
福飯　雄太 <yuta@fukuhanyuutanoMacBook-Air.local>
2020-05-11 16:53:16 +0900

    install

--- vue/src/main.js
+++ vue/src/main.js
@@ -2,11 +2,16 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import Vuetify from 'vuetify'
+import "vuetify/dist/vuetify.min.css";
+
+
 
 Vue.config.productionTip = false
 
 new Vue({
   router,
   store,
+  Vuetify,
   render: function (h) { return h(App) }
 }).$mount('#app')
---
+++ vue/src/views/Submit.vue
@@ -0,0 +1,21 @@
+<template>
+  <div >
+    <button src="../">
+      戻る
+    </button>
+    <button>
+      登録
+    </button>
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'Submit',
+  components: {
+    Submit
+  },
+}
+</script>
