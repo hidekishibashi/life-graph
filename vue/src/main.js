4e9fb5743ff4b9f214f5f7075862c993f19b1785
mei kishida <mei.kishida@seattleconsulting.co.jp>
2020-05-13 19:52:20 +0900

    【topモック作成】

--- vue/src/main.js
+++ vue/src/main.js
@@ -2,10 +2,6 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-<<<<<<< HEAD
-import Vuetify from 'vuetify'
-=======
->>>>>>> develop
 import vuetify from './plugins/vuetify'
 // import "vuetify/dist/vuetify.min.css";
 
@@ -14,10 +10,6 @@ Vue.config.productionTip = false
 new Vue({
   router,
   store,
-<<<<<<< HEAD
-  Vuetify,
-=======
->>>>>>> develop
   vuetify,
   render: function (h) { return h(App) }
 }).$mount('#app')
