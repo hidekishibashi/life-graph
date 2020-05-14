d3a3c563101eed6971bb45dcb2df7a5c65a87208
mei kishida <mei.kishida@seattleconsulting.co.jp>
2020-05-14 15:09:11 +0900

    【Top画面インデント修正

--- vue/src/views/Reference.vue
+++ vue/src/views/Reference.vue
@@ -25,16 +25,18 @@ export default {
 </script>
 
 <style>
-#returnBtn    {background: #FFFFFF;
-              font-family: Roboto;
-              font-style: normal;
-              font-weight: 500;
-              font-size: 18px;
-              line-height: 21px;
-              /* identical to box height */
-              color: #26A69A;}
+#returnBtn    {
+  background: #FFFFFF;
+  font-family: Roboto;
+  font-style: normal;
+  font-weight: 500;
+  font-size: 18px;
+  line-height: 21px;
+  /* identical to box height */
+  color: #26A69A;}
 
-#refChart     {width: 300px;
-              height: 300px;
-              margin: 0 auto;}
+#refChart     {
+  width: 300px;
+  height: 300px;
+  margin: 0 auto;}
 </style>
--- vue/src/views/Top.vue
+++ vue/src/views/Top.vue
@@ -125,169 +125,184 @@ export default {
 // }
 </script>
 <style>
-#topChart {position:absolute;
-          top:400px;
-          right:200px;}
+#topChart {
+  position:absolute;
+  top:400px;
+  right:200px;}
 
-#topSearchBef   {position: absolute;
-                width: 404px;
-                height: 264px;
-                left: 263px;
-                top: 231px;
+#topSearchBef   {
+  position: absolute;
+  width: 404px;
+  height: 264px;
+  left: 263px;
+  top: 231px;
 
-                background: #26A69A;
-                box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
-                border-radius: 2px;}
+  background: #26A69A;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  border-radius: 2px;}
 
-#topSearchAft   {position: absolute;
-                width: 404px;
-                height: 264px;
-                left: 263px;
-                top: 231px;
+#topSearchAft   {
+  position: absolute;
+  width: 404px;
+  height: 264px;
+  left: 263px;
+  top: 231px;
 
-                background: #26A69A;
-                box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
-                border-radius: 2px;}
+  background: #26A69A;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  border-radius: 2px;}
 
-#topEntry   {position: absolute;
-            width: 404px;
-            height: 264px;
-            left: 769px;
-            top: 231px;
+#topEntry   {
+  position: absolute;
+  width: 404px;
+  height: 264px;
+  left: 769px;
+  top: 231px;
 
-            background: #FFFFFF;
-            border: 2px solid #26A69A;
-            box-sizing: border-box;
-            box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
-            border-radius: 2px;}
+  background: #FFFFFF;
+  border: 2px solid #26A69A;
+  box-sizing: border-box;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  border-radius: 2px;}
 
-#topEdit    {position: absolute;
-            width: 404px;
-            height: 264px;
-            left: 769px;
-            top: 231px;
+#topEdit    {
+  position: absolute;
+  width: 404px;
+  height: 264px;
+  left: 769px;
+  top: 231px;
 
-            background: #FFFFFF;
-            border: 2px solid #26A69A;
-            box-sizing: border-box;
-            box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
-            border-radius: 2px;}
+  background: #FFFFFF;
+  border: 2px solid #26A69A;
+  box-sizing: border-box;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  border-radius: 2px;}
 
-.searchTitle  {position: absolute;
-              width: 139px;
-              height: 42px;
-              left: 0px;
-              top: 20px;
+.searchTitle  {
+  position: absolute;
+  width: 139px;
+  height: 42px;
+  left: 0px;
+  top: 20px;
 
-              font-family: Roboto;
-              font-style: normal;
-              font-weight: bold;
-              font-size: 36px;
-              line-height: 42px;
+  font-family: Roboto;
+  font-style: normal;
+  font-weight: bold;
+  font-size: 36px;
+  line-height: 42px;
 
-              color: #FFFFFF;
-              }
+  color: #FFFFFF;
+  }
 
-#entryTitle   {position: absolute;
-              width: 139px;
-              height: 42px;
-              left: 0px;
-              top: 20px;
+#entryTitle   {
+  position: absolute;
+  width: 139px;
+  height: 42px;
+  left: 0px;
+  top: 20px;
 
-              font-family: Roboto;
-              font-style: normal;
-              font-weight: bold;
-              font-size: 36px;
-              line-height: 42px;
+  font-family: Roboto;
+  font-style: normal;
+  font-weight: bold;
+  font-size: 36px;
+  line-height: 42px;
 
-              color: #777575;
-              }
+  color: #777575;
+  }
 
-#editTitle      {position: absolute;
-                width: 139px;
-                height: 42px;
-                left: 0px;
-                top: 20px;
+#editTitle      {
+  position: absolute;
+  width: 139px;
+  height: 42px;
+  left: 0px;
+  top: 20px;
 
-                font-family: Roboto;
-                font-style: normal;
-                font-weight: bold;
-                font-size: 36px;
-                line-height: 42px;
+  font-family: Roboto;
+  font-style: normal;
+  font-weight: bold;
+  font-size: 36px;
+  line-height: 42px;
 
-                color: #777575;}
+  color: #777575;}
 
-.searchSubtitle {position: absolute;
-                width: 353px;
-                height: 56px;
-                left: 0px;
-                top: 100px;
+.searchSubtitle {
+  position: absolute;
+  width: 353px;
+  height: 56px;
+  left: 0px;
+  top: 100px;
 
-                font-family: Roboto;
-                font-style: normal;
-                font-weight: 500;
-                font-size: 24px;
-                line-height: 28px;
+  font-family: Roboto;
+  font-style: normal;
+  font-weight: 500;
+  font-size: 24px;
+  line-height: 28px;
 
-                color: #FFFFFF;
-                }
+  color: #FFFFFF;
+  }
 
-#entrySubtitle  {position: absolute;
-                width: 353px;
-                height: 56px;
-                left: 0px;
-                top: 100px;
+#entrySubtitle  {
+  position: absolute;
+  width: 353px;
+  height: 56px;
+  left: 0px;
+  top: 100px;
 
-                font-family: Roboto;
-                font-style: normal;
-                font-weight: 500;
-                font-size: 24px;
-                line-height: 28px;
+  font-family: Roboto;
+  font-style: normal;
+  font-weight: 500;
+  font-size: 24px;
+  line-height: 28px;
 
-                color: #777575;}
+  color: #777575;}
 
-#editSubtitle   {position: absolute;
-                width: 353px;
-                height: 56px;
-                left: 0px;
-                top: 100px;
+#editSubtitle   {
+  position: absolute;
+  width: 353px;
+  height: 56px;
+  left: 0px;
+  top: 100px;
 
-                font-family: Roboto;
-                font-style: normal;
-                font-weight: 500;
-                font-size: 24px;
-                line-height: 28px;
+  font-family: Roboto;
+  font-style: normal;
+  font-weight: 500;
+  font-size: 24px;
+  line-height: 28px;
 
-                color: #777575;}
+  color: #777575;}
 
-.searchBtn    {position: absolute;
-              width: 124px;
-              height: 52px;
-              left: 210px;
-              top: 110px;
+.searchBtn    {
+  position: absolute;
+  width: 124px;
+  height: 52px;
+  left: 210px;
+  top: 110px;
 
-              background: #FFFFFF;
-              border-radius: 2px;}
+  background: #FFFFFF;
+  border-radius: 2px;}
 
-#entryBtn     {position: absolute;
-              width: 124px;
-              height: 52px;
-              left: 200px;
-              top: 200px;
+#entryBtn     {
+  position: absolute;
+  width: 124px;
+  height: 52px;
+  left: 200px;
+  top: 200px;
 
-              background: #64D8CB;
-              border-radius: 2px;}
+  background: #64D8CB;
+  border-radius: 2px;}
 
-#editBtn      {position: absolute;
-              width: 124px;
-              height: 52px;
-              left: 200px;
-              top: 200px;
+#editBtn      {
+  position: absolute;
+  width: 124px;
+  height: 52px;
+  left: 200px;
+  top: 200px;
 
-              background: #64D8CB;
-              border-radius: 2px;}
+  background: #64D8CB;
+  border-radius: 2px;}
 
-#topChart     {width: 300px;
-              height: 300px;
-              margin: 0 auto;}
+#topChart     {
+  width: 300px;
+  height: 300px;
+  margin: 0 auto;}
 </style>
