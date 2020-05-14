13ad128b50ec80c81c264fda15714dabfa2fbfcd
reiki-shimoharaguchi <reiki.shimoharaguchi@seattleconsulting.co.jp>
2020-05-14 22:12:48 +0900

    Fix

--- vue/src/views/Login.vue
+++ vue/src/views/Login.vue
@@ -16,7 +16,6 @@
           <!-- mdi-eye-offの部分は時間がなければなくします -->
           <v-text-field type="password" prepend-icon="mdi-lock" append-icon="mdi-eye-off" label="パスワード" />
           <v-card-actions>
-<<<<<<< HEAD
             <!-- ボタンのカラー変更は調査中 -->
             <!-- <v-btn color="teal">新規登録</v-btn> -->
             <!-- <v-btn outline color="#26A69A">新規登録</v-btn> -->
@@ -25,13 +24,11 @@
             </v-btn>
             <v-spacer />
             <v-btn large color="#26A69A" class="log-btn" to="/Top">
-=======
             <v-btn outlined large color="#26A69A" to="/signup">
               新規登録
             </v-btn>
             <v-spacer />
             <v-btn large color="#26A69A" class="log-btn" to="/top">
->>>>>>> 5b69ddc51dfc0bebc8cdc9d1606d3cf93a33bf68
               ログイン
             </v-btn>
             <!-- <v-btn  color="pink">ログイン</v-btn> -->
@@ -45,7 +42,6 @@
 <style scoped>
 </style>
 <script>
-<<<<<<< HEAD
 export default {
   layout: 'blank', // layouts/blank.vueを使用
   middleware: ['auth'],
@@ -80,7 +76,6 @@ export default {
     }
   }
 }
-=======
 // sriptはブラッシュアップに回します
 // export default {
 //   layout: 'blank', // layouts/blank.vueを使用
@@ -118,17 +113,12 @@ export default {
 //     }
 //   }
 // }
->>>>>>> 5b69ddc51dfc0bebc8cdc9d1606d3cf93a33bf68
 </script>
 <style>
 .logo{
   text-align: center;
   margin: 60px;
 }
-<<<<<<< HEAD
-=======
-
->>>>>>> 5b69ddc51dfc0bebc8cdc9d1606d3cf93a33bf68
 /* ログインボタンの文字色 */
 .theme--light.v-btn:not(.v-btn--flat):not(.v-btn--text):not(.v-btn--outlined){
   color:white;
