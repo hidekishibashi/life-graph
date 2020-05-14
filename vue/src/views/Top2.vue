c7d6ac03c43e231518dbcec80d1abebd71e19487
mei kishida <mei.kishida@seattleconsulting.co.jp>
2020-05-14 21:38:49 +0900

    【top画面参照画面】20200514ver3

--- vue/src/views/Top2.vue
+++ vue/src/views/Top2.vue
@@ -1,41 +1,54 @@
 <template>
-  <div id="reference">
-    <router-link to="">
-      <v-btn id="returnBtn">
-        戻る
-      </v-btn>
-    </router-link>
-    <div id="refChart">
-      <Chart />
-    </div>
-  </div>
+  <!-- ログイン前の画面 -->
+  <v-app id="top">
+    <v-card
+      id="topSearchBef"
+      class="mx-auto"
+      max-width="344"
+      outlined
+    >
+      <v-list-item three-line>
+        <v-list-item-content>
+          <v-list-item-title class="searchTitle">
+            検索
+          </v-list-item-title>
+          <v-list-item-subtitle class="searchSubtitle">
+            希望の人生グラフを見ることができます。
+          </v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
+      <v-card-actions>
+        <router-link to="/search">
+          <v-btn class="searchBtn" text>
+            検索する
+          </v-btn>
+        </router-link>
+      </v-card-actions>
+    </v-card>
+    <v-card
+      id="topEntry"
+      class="mx-auto"
+      max-width="344"
+      outlined
+    >
+      <v-list-item three-line>
+        <v-list-item-content>
+          <v-list-item-title id="entryTitle">
+            登録
+          </v-list-item-title>
+          <v-list-item-subtitle id="entrySubtitle">
+            人生グラフを作成できます。
+          </v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
+      <v-card-actions>
+        <router-link to="/new">
+          <v-btn id="entryBtn" text>
+            登録する
+          </v-btn>
+        </router-link>
+      </v-card-actions>
+    </v-card>
+    <!-- </p> -->
+  </v-app>
 </template>
-<script>
-import Chart from '../chart/Chart'
-export default {
-  name: 'Reference',
-  components: {
-    Chart
-  },
-  data: () => ({
-    //
-  })
-}
-</script>
-
-<style>
-#returnBtn    {
-  background: #FFFFFF;
-  font-style: normal;
-  font-weight: 500;
-  font-size: 18px;
-  line-height: 21px;
-  /* identical to box height */
-  color: #26A69A;
-}
-#refChart     {
-  width: 300px;
-  height: 300px;
-  margin: 0 auto;
-}
-</style>
