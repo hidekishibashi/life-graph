843ad2579aeb21c000cdb175dc619f1985cce8c2
Naoya-Hayashi <62989057+Naoya-Hayashi@users.noreply.github.com>
2020-05-15 18:25:08 +0900

    pom chenged

--- api/bin/src/main/resources/db/migration/V2__UserInsert.sql
+++
@@ -1,9 +0,0 @@
-INSERT INTO `User` (
-  `name`,
-  `password`,
-  `mail_address`
-) VALUES
-  ('taro', '12345', 'seattle'),
-  ('Hanako', 'pass', 'academy'),
-  ('Yamada', '12345', 'mountain'),
-  ('Taro', 'Gogogo', 'desk');
