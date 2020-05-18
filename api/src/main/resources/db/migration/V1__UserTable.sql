a72b28fb710217712f66c978d2ee98232404dcf6
reiki-shimoharaguchi <62986428+reiki-shimoharaguchi@users.noreply.github.com>
2020-05-18 20:37:48 +0900

    Update V1__UserTable.sql

--- api/src/main/resources/db/migration/V1__UserTable.sql
+++ api/src/main/resources/db/migration/V1__UserTable.sql
@@ -14,7 +14,6 @@ CREATE TABLE IF NOT EXISTS `users` (
   `username` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
   `password` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
   `email` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
---  `role` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY (`email`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
