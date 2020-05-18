13b99981538e79c9d9ca26216570b7e1cd7d1cb2
reiki-shimoharaguchi <reiki.shimoharaguchi@seattleconsulting.co.jp>
2020-05-18 20:01:50 +0900

    Login API

--- api/src/main/resources/db/migration/V1__UserTable.sql
+++ api/src/main/resources/db/migration/V1__UserTable.sql
@@ -7,3 +7,50 @@ CREATE TABLE IF NOT EXISTS `User` (
   PRIMARY KEY (`id`),
   UNIQUE KEY (`mail_address`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `username` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `password` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+--  `role` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY (`email`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+CREATE TABLE `roles` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(20) NOT NULL,
+  PRIMARY KEY (`id`)
+)ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=UTF8MB4;
+
+
+CREATE TABLE `user_roles` (
+  `user_id` bigint NOT NULL,
+  `role_id` int NOT NULL,
+  PRIMARY KEY (`user_id`),
+  FOREIGN KEY (`user_id`) REFERENCES `users` (`id`),
+  FOREIGN KEY (`role_id`) REFERENCES `roles` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=UTF8MB4;
+
+
+INSERT INTO `users` (
+  `username`,
+  `password`,
+  `email`
+--  `role`
+) VALUES
+  ('taro', '12345', 'seattle'),
+  ('Hanako', 'pass', 'academy'),
+  ('Yamada', '12345', 'mountain'),
+  ('Taro', 'Gogogo', 'desk');
+
+
+INSERT INTO `roles` (
+  `name`
+) VALUES
+  ('ROLE_USER'),
+  ('ROLE_MODERATOR'),
+  ('ROLE_ADMIN');
+
