319bc1d9606ebc74af0d86c560770c2af3c6b5e4
福飯　雄太 <yuta@fukuhanyuutanoMacBook-Air.local>
2020-05-18 17:52:01 +0900

    add_resisterAPI

---
+++ api/src/main/java/com/lifegraph/team20/models/ParentLifeGraph.java
@@ -0,0 +1,63 @@
+package com.lifegraph.team20.models;
+
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
+import javax.validation.constraints.NotBlank;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import lombok.Data;
+
+@Data
+@Entity
+@Table (name = "Parent_Chart",
+		uniqueConstraints = {
+		@UniqueConstraint(columnNames = "user_id")})
+
+public class ParentLifeGraph {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+	private long id;
+
+	@NotBlank
+	@Column(name = "user_id")
+	private long user_id;
+
+	@NotBlank
+	@Column(name = "created_at")
+	protected java.sql.Timestamp createdAtTime;
+
+	@NotBlank
+	@Column(name = "updated_at")
+	protected java.sql.Timestamp updatedAtTime;
+
+	@OneToMany(fetch = FetchType.LAZY, mappedBy = "parent", orphanRemoval=true)
+	@JsonIgnore
+	static
+     List<LifeGraph> lifeGraph;
+
+	public ParentLifeGraph(long user_id, java.sql.Timestamp createdAtTime, java.sql.Timestamp updatedAtTime) {
+		this.user_id = user_id;
+		this.createdAtTime = createdAtTime;
+		this.updatedAtTime = updatedAtTime;
+	}
+
+	public static List<LifeGraph> getLifeGraph() {
+		return lifeGraph;
+	}
+
+	public void setLifeGraph(List<LifeGraph> lifeGraph) {
+		this.lifeGraph = lifeGraph;
+	}
+}
+
--- api/src/main/resources/db/migration/V2__UserInsert.sql
+++ api/src/main/resources/db/migration/V2__UserInsert.sql
@@ -7,3 +7,26 @@ INSERT INTO `User` (
   ('Hanako', 'pass', 'academy'),
   ('Yamada', '12345', 'mountain'),
   ('Taro', 'Gogogo', 'desk');
+
+--   CREATE TABLE `Parent_Chart` (
+--   `id` bigint NOT NULL AUTO_INCREMENT,
+--   `user_id` bigint NOT NULL,
+--   `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+--   `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+--   PRIMARY KEY (`id`),
+--   UNIQUE KEY `user_id` (`user_id`),
+--   CONSTRAINT `Parent_Chart_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `User` (`id`)
+-- ) ENGINE=InnoDB AUTO_INCREMENT=26 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- CREATE TABLE `Child_Chart` (
+--   `id` bigint NOT NULL AUTO_INCREMENT,
+--   `parent_id` bigint NOT NULL,
+--   `score` int NOT NULL,
+--   `age` int NOT NULL,
+--   `comment` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+--   PRIMARY KEY (`id`),
+--   UNIQUE KEY `parent_id` (`parent_id`,`age`),
+--   UNIQUE KEY `UKqyrxsc9ri7kkiv4kckekopnny` (`parent_id`),
+--   UNIQUE KEY `UKm7wu6d4f1wmp2phfchl22gv6h` (`age`),
+--   CONSTRAINT `child_chart_ibfk_1` FOREIGN KEY (`parent_id`) REFERENCES `Parent_Chart` (`id`)
+-- ) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
