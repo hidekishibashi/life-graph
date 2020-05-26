5aba137b32fc20f1e5267242370882cbe4ddeb6a
福飯　雄太 <yuta@fukuhanyuutanoMacBook-Air.local>
2020-05-26 11:13:23 +0900

    fix_resister

--- vue/src/store/modules/chart.js
+++ vue/src/store/modules/chart.js
@@ -17,7 +17,7 @@ export default {
   },
   actions: {
     async setContents ({ commit }, userId) {
-      const url = '/api/auth/reference?userID=' + userId
+      const url = 'api/auth/reference/' + userId
       await axios.get(url).then((res) => {
         commit('setContents', res.data)
       })
