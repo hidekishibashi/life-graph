9847175098938bae706332acd07504cfdd869823
福飯　雄太 <yuta@fukuhanyuutanoMacBook-Air.local>
2020-05-26 19:50:57 +0900

    ごめんわからん

--- vue/src/store/index.js
+++ vue/src/store/index.js
@@ -5,6 +5,7 @@ import chart from './modules/chart.js'
 import account from './modules/account.js'
 import refUser from './modules/refUser.js'
 import search from './modules/search.js'
+import createPersistedState from 'vuex-persistedstate'
 
 // import axios from 'axios'
 Vue.use(Vuex)
@@ -15,5 +16,9 @@ export default new Vuex.Store({
     account,
     refUser,
     search
-  }
+  },
+  plugins: [createPersistedState({
+    key: 'example',
+    storage: window.sessionStorage
+  })]
 })
